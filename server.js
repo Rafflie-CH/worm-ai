d7d35501cef73dbbdb51606a65aba2195f321550
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-14 11:08:19 +0800

    Fix static file serving path in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
