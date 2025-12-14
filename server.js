b28af02c92995dbb1ac4df5554a7e91214c2541b
Rafflie-CH <rafflieaditya1234@gmail.com>
2025-12-14 11:05:46 +0800

    Enhance server.js to serve index.html
    Add path and url modules to serve static files.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import "dotenv/config";
 import express from "express";
 import fetch from "node-fetch";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const sessions = new Map();
 
